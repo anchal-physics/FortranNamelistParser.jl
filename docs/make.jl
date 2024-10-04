e9823b086b3e418da10eed51d745ed5fa62e1bb6
Anchal Gupta <guptaa@fusion.gat.com>
2024-10-04 16:32:05 -0700

    Conforms to format

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,9 @@
 using Fortran90Namelists
 using Documenter
 
-DocMeta.setdocmeta!(Fortran90Namelists, :DocTestSetup, :(using Fortran90Namelists); recursive=true)
+DocMeta.setdocmeta!(
+    Fortran90Namelists, :DocTestSetup, :(using Fortran90Namelists); recursive=true
+)
 
 makedocs(;
     modules=[Fortran90Namelists],
@@ -16,9 +18,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Manual" => [
-            "Installation guide" => "installation.md",
-        ],
+        "Manual" => ["Installation guide" => "installation.md"],
         "API Reference" => "api.md",
         "Developer Docs" => [
             "Contributing" => "developers/contributing.md",
